4d5153c51b70c02dcd2d0219b7116c5fa62d93f3
root <root@simply-vc.com.mt>
2022-10-14 18:52:55 +0200

    Fixed failing test

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,7 +1,13 @@
 package keeper_test
 
 import (
+	"context"
+
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/x/checkers"
+	"github.com/alice/checkers/x/checkers/keeper"
 	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -12,15 +18,21 @@ const (
 	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
-	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+	msgServer, _, ctx := setupMsgServerCreateGame(t)
+	createResponse, err := msgServer.CreateGame(ctx, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
 		Red:     carol,
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "", // TODO: update with a proper value when updated
+		GameIndex: "1",
 	}, *createResponse)
 }
