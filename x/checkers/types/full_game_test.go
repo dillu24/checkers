7d575679c3b26d676c17518fc697a8f117e1d2de
root <root@simply-vc.com.mt>
2022-10-14 09:35:07 +0200

    Added some unit tests all the remaining game util functions

--- x/checkers/types/full_game_test.go
+++ x/checkers/types/full_game_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
@@ -47,4 +48,66 @@ func TestGetAddressWrongBlack(t *testing.T) {
 			bad_address),
 	)
 	require.EqualError(t, storedGame.Validate(), actualAddrErr.Error())
+}
+
+func TestCanGetAddressRed(t *testing.T) {
+	expectedAddress, expectedAddrErr := sdk.AccAddressFromBech32(bob)
+	actualAddress, actualAddrErr := GetStoredGame1().GetRedAddress()
+	require.Equal(t, expectedAddress, actualAddress)
+	require.Nil(t, expectedAddrErr)
+	require.Nil(t, actualAddrErr)
+}
+
+func TestGetAddressWrongRed(t *testing.T) {
+	storedGame := GetStoredGame1()
+	storedGame.Red = bad_address
+	actualAddress, actualAddrErr := storedGame.GetRedAddress()
+	require.Nil(t, actualAddress)
+	require.EqualError(
+		t,
+		actualAddrErr,
+		fmt.Sprintf(
+			"red address is invalid: %s: decoding bech32 failed: invalid separator index -1",
+			bad_address),
+	)
+	require.EqualError(t, storedGame.Validate(), actualAddrErr.Error())
+}
+
+func TestParseGameCorrect(t *testing.T) {
+	game, err := GetStoredGame1().ParseGame()
+	require.EqualValues(t, rules.New(), game)
+	require.Nil(t, err)
+}
+
+func TestParseGameIfChangedOk(t *testing.T) {
+	storedGame := GetStoredGame1()
+	storedGame.Board = strings.Replace(storedGame.Board, "b", "r", 1)
+	game, err := storedGame.ParseGame()
+	require.NotEqualValues(t, rules.New(), game)
+	require.Nil(t, err)
+}
+
+func TestParseGameWrongPieceColor(t *testing.T) {
+	storedGame := GetStoredGame1()
+	storedGame.Board = strings.Replace(storedGame.Board, "b", "w", 1)
+	game, err := storedGame.ParseGame()
+	require.Nil(t, game)
+	require.NotNil(t, err)
+	require.EqualError(t, err, "game cannot be parsed: invalid board, invalid piece at 1, 0")
+	require.EqualError(t, storedGame.Validate(), err.Error())
+}
+
+func TestParseGameWrongTurnColor(t *testing.T) {
+	storedGame := GetStoredGame1()
+	storedGame.Turn = "w"
+	game, err := storedGame.ParseGame()
+	require.Nil(t, game)
+	require.NotNil(t, err)
+	require.EqualError(t, err, fmt.Sprintf("game cannot be parsed: Turn: %s", storedGame.Turn))
+	require.EqualError(t, storedGame.Validate(), err.Error())
+}
+
+func TestValidateOk(t *testing.T) {
+	storedGame := GetStoredGame1()
+	require.NoError(t, storedGame.Validate())
 }
