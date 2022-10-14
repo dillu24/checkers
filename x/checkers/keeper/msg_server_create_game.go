c33fcff7a3f8a988f564f6309703c99fabbd71c3
root <root@simply-vc.com.mt>
2022-10-14 18:22:19 +0200

    Added handling for createGame message

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,16 +2,39 @@ package keeper
 
 import (
 	"context"
+	"github.com/alice/checkers/x/checkers/rules"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"strconv"
 
 	"github.com/alice/checkers/x/checkers/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found!")
+	}
+	nextIndex := strconv.FormatUint(systemInfo.NextId, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: nextIndex,
+		Board: newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Black: msg.Black,
+		Red:   msg.Red,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgCreateGameResponse{GameIndex: nextIndex}, nil
 }
