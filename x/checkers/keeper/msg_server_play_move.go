4b275fca345353aecff6cc9442d1ceb740d0861c
root <root@simply-vc.com.mt>
2022-10-15 12:30:08 +0200

    Added playMove msg handling

--- x/checkers/keeper/msg_server_play_move.go
+++ x/checkers/keeper/msg_server_play_move.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"github.com/alice/checkers/x/checkers/rules"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,8 +12,53 @@ import (
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.GameIndex)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "%s", msg.GameIndex)
+	}
 
-	return &types.MsgPlayMoveResponse{}, nil
+	isBlack := storedGame.Black == msg.Creator
+	isRed := storedGame.Red == msg.Creator
+	var player rules.Player
+	if isBlack && isRed {
+		player = rules.StringPieces[storedGame.Turn].Player
+	} else if isBlack {
+		player = rules.BLACK_PLAYER
+	} else if isRed {
+		player = rules.RED_PLAYER
+	} else {
+		return nil, sdkerrors.Wrapf(types.ErrCreatorNotPlayer, "%s", msg.Creator)
+	}
+
+	game, err := storedGame.ParseGame()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if !game.TurnIs(player) {
+		return nil, sdkerrors.Wrapf(types.ErrNotPlayerTurn, "%s", player)
+	}
+
+	src := rules.Pos{
+		X: int(msg.FromX),
+		Y: int(msg.FromY),
+	}
+	dst := rules.Pos{
+		X: int(msg.ToX),
+		Y: int(msg.ToY),
+	}
+	captured, err := game.Move(src, dst)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(types.ErrWrongMove, err.Error())
+	}
+
+	storedGame.Board = game.String()
+	storedGame.Turn = rules.PieceStrings[game.Turn]
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	return &types.MsgPlayMoveResponse{
+		CapturedX: int32(captured.X),
+		CapturedY: int32(captured.Y),
+		Winner:    rules.PieceStrings[game.Winner()],
+	}, nil
 }
