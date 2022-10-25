d6b090ec3323c418b5e94d5db6111ae643c7b769
root <root@simply-vc.com.mt>
2022-10-25 16:05:41 +0200

    Added fifo specific test to msg_server_reject_game

---
+++ x/checkers/keeper/msg_server_reject_game_fifo_test.go
@@ -0,0 +1,91 @@
+package keeper_test
+
+import (
+	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRejectFirstGameHasSavedFifo(t *testing.T) {
+	msgServer, k, context := setupMsgServerWithOneGameForRejectGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Black:   carol,
+		Red:     alice,
+	})
+	msgServer.RejectGame(context, &types.MsgRejectGame{
+		Creator:   carol,
+		GameIndex: "1",
+	})
+	systemInfo, found := k.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        3,
+		FifoHeadIndex: "2",
+		FifoTailIndex: "2",
+	}, systemInfo)
+	game2, found := k.GetStoredGame(ctx, "2")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "2",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       carol,
+		Red:         alice,
+		MoveCount:   0,
+		BeforeIndex: types.NoFifoIndex,
+		AfterIndex:  types.NoFifoIndex,
+	}, game2)
+}
+
+func TestRejectMiddleGameHasSaved(t *testing.T) {
+	msgServer, k, context := setupMsgServerWithOneGameForRejectGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Black:   carol,
+		Red:     alice,
+	})
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Black:   alice,
+		Red:     bob,
+	})
+	msgServer.RejectGame(context, &types.MsgRejectGame{
+		Creator:   carol,
+		GameIndex: "2",
+	})
+	systemInfo, found := k.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        4,
+		FifoHeadIndex: "1",
+		FifoTailIndex: "3",
+	}, systemInfo)
+	game1, found := k.GetStoredGame(ctx, "1")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "1",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       bob,
+		Red:         carol,
+		MoveCount:   0,
+		BeforeIndex: types.NoFifoIndex,
+		AfterIndex:  "3",
+	}, game1)
+	game3, found := k.GetStoredGame(ctx, "3")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index:       "3",
+		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:        "b",
+		Black:       alice,
+		Red:         bob,
+		MoveCount:   0,
+		BeforeIndex: "1",
+		AfterIndex:  types.NoFifoIndex,
+	}, game3)
+}
