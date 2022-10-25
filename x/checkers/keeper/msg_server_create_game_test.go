9e272ef490437a2a8b88460c65c03aa3c59c33e6
root <root@simply-vc.com.mt>
2022-10-25 15:06:29 +0200

    Revert "Added fifo specific test to msg_server_create_game"
    This reverts commit 67c214c91cd132a540af75ab348d2c4be0303c0c.

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -171,7 +171,7 @@ func TestCreate3Games(t *testing.T) {
 	}, createResponse)
 }
 
-func TestCreate3GamesHasSavedFiFo(t *testing.T) {
+func TestCreate3GamesHasSaved(t *testing.T) {
 	msgSrvr, k, context := setupMsgServerCreateGame(t)
 	ctx := sdk.UnwrapSDKContext(context)
 	msgSrvr.CreateGame(context, &types.MsgCreateGame{
@@ -203,18 +203,6 @@ func TestCreate3GamesHasSavedFiFo(t *testing.T) {
 	systemInfo, found = k.GetSystemInfo(ctx)
 	require.True(t, found)
 	require.EqualValues(t, types.SystemInfo{NextId: 3, FifoHeadIndex: "1", FifoTailIndex: "2"}, systemInfo)
-	storedGame, found = k.GetStoredGame(ctx, "1")
-	require.True(t, found)
-	require.EqualValues(t, types.StoredGame{
-		Index:       "1",
-		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:        "b",
-		Black:       bob,
-		Red:         carol,
-		MoveCount:   0,
-		BeforeIndex: types.NoFifoIndex,
-		AfterIndex:  "2",
-	}, storedGame)
 	storedGame, found = k.GetStoredGame(ctx, "2")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
@@ -236,30 +224,6 @@ func TestCreate3GamesHasSavedFiFo(t *testing.T) {
 	systemInfo, found = k.GetSystemInfo(ctx)
 	require.True(t, found)
 	require.EqualValues(t, types.SystemInfo{NextId: 4, FifoHeadIndex: "1", FifoTailIndex: "3"}, systemInfo)
-	storedGame, found = k.GetStoredGame(ctx, "1")
-	require.True(t, found)
-	require.EqualValues(t, types.StoredGame{
-		Index:       "1",
-		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:        "b",
-		Black:       bob,
-		Red:         carol,
-		MoveCount:   0,
-		BeforeIndex: types.NoFifoIndex,
-		AfterIndex:  "2",
-	}, storedGame)
-	storedGame, found = k.GetStoredGame(ctx, "2")
-	require.True(t, found)
-	require.EqualValues(t, types.StoredGame{
-		Index:       "2",
-		Board:       "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:        "b",
-		Black:       alice,
-		Red:         bob,
-		MoveCount:   0,
-		BeforeIndex: "1",
-		AfterIndex:  "3",
-	}, storedGame)
 	storedGame, found = k.GetStoredGame(ctx, "3")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
